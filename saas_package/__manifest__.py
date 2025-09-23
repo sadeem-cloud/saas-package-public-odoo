710f1b6c8499d038630c3d56bf3fa75c22883e0a
elias <info@sadeem.cloud>
2025-09-23 15:23:59 +0300

    1

---
+++ saas_package/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-).get_param
+{
+    'name': "Sadeem SaaS Package",
+    'summary': """
+    Transform Odoo into Professional SaaS Platform   
+        """,
+    'description': """
+    Sadeem SaaS module allows you to transform your Odoo application
+    into a professional SaaS platform just like Odoo Online. The module uses Docker/DBfilter Solutions.
+    More info can be found here,
+    https://sadeem.cloud/saas-package. 
+    """,
+    'author': "SADEEM",
+    'website': "https://sadeem.cloud",
+    'category': 'Technical',
+    'version': '1.250917',
+    'depends': ['base'],
+    'data': [
+    ],
+    'price': 6000.00,
+    'currency': 'EUR',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'OPL-1',
+    'images': ['static/description/banner.gif'],
+}
